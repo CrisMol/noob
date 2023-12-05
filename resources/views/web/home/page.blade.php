be84e747c0b2ce75c1d8e7ed69b3b8892459c9ac
DESKTOP-7CIH4NL\GAMER <molinacuario_97@hotmail.com>
2023-12-05 22:10:40 +0100

    Inicio del proyecto

---
+++ resources/views/web/home/page.blade.php
@@ -0,0 +1,13 @@
+@extends('web.layouts.app')
+
+@section('head')
+    @include('web.home.section.head')
+@endsection
+
+@section('content')
+    @include('web.home.section.body')
+@endsection
+
+@section('javascript')
+    @include('web.home.section.footer')
+@endsection
---
+++ resources/views/web/home/section/body.blade.php
@@ -0,0 +1,12 @@
+<!--header-->
+<header>
+    <a href="#" class="logo">Noob.</a>
+    <nav class="menu">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
